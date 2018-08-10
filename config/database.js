e8c6107de970f8aaa11762169442bb0447daba14
Daniel Stieber <dstieber@cs.csubak.edu>
2018-08-10 14:20:22 -0700

    fixed db connection

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://daniel:DEf-7PG-Q8g-8ve@ds219432.mlab.com:19432/meanauthapp',
+  database: 'mongodb://daniel:8Ae-czz-mzt-wxF@ds219432.mlab.com:19432/meanauthapp',
   secret: 'yoursecret',
 };
